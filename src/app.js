ec172d39c679108f7fba2e6778a6de9ee7638d9d
TyDine22 <150012901+TyDine22@users.noreply.github.com>
2023-12-28 09:27:49 +0000

    make contact me page functional, added a submiited page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -15,6 +21,45 @@ app.get('/about', (req,res) => {
 
 app.get('/contact', (req,res) => {
     res.sendFile(path.join(__dirname, 'pages/contact.html'));
+})
+
+app.get('/send-email', (req,res) => {
+    res.sendFile(path.join(__dirname, 'pages/submitted.html'));
+})
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASSWORD
+    }
+})
+
+app.post('/send-email', (req,res) => {
+    const { name, email, coverletter } = req.body;
+
+    const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: process.env.EMAIL_ID,
+        subject: 'Message from personal website',
+        html:`
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Message:</strong> ${coverletter}</p>
+            `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.redirect('/send-email')
+        }
+    });
 })
 
 const port = process.env.PORT || 3000;
