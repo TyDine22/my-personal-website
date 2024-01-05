b127a92999b8b5ec042b2fb346c9917362124a02
TyDine22 <150012901+TyDine22@users.noreply.github.com>
2024-01-05 09:05:58 +0000

    implemented human verification to the contact page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
@@ -37,29 +39,42 @@ const transporter = nodemailer.createTransport({
     }
 })
 
-app.post('/send-email', (req,res) => {
-    const { name, email, coverletter } = req.body;
+app.post('/send-email', async (req,res) => {
+    const { name, email, coverletter, 'g-recaptcha-response': gRecaptchaResponse } = req.body;
 
-    const mailOptions = {
-        from: process.env.EMAIL_ID,
-        to: process.env.EMAIL_ID,
-        subject: 'Message from personal website',
-        html:`
-            <p><strong>Name:</strong> ${name}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Message:</strong> ${coverletter}</p>
-            `
-    };
+    const recaptchaSecretKey = '6Lf0zUYpAAAAAF9-rJQMjnoFqLWQG6BqGus_Zyeh';
+    const recaptchaVerifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${gRecaptchaResponse}`;
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(error);
-            res.status(500).send('Error sending email');
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.redirect('/send-email')
+    try {
+        const recaptchaResponse = await axios.post(recaptchaVerifyUrl);
+        if (!recaptchaResponse.data.success) {
+            return res.status(400).send('reCAPTCHA verification failed');
         }
-    });
+
+        const mailOptions = {
+            from: process.env.EMAIL_ID,
+            to: process.env.EMAIL_ID,
+            subject: 'Message from personal website',
+            html:`
+                <p><strong>Name:</strong> ${name}</p>
+                <p><strong>Email:</strong> ${email}</p>
+                <p><strong>Message:</strong> ${coverletter}</p>
+                `
+        };
+    
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+                res.status(500).send('Error sending email');
+            } else {
+                console.log('Email sent: ' + info.response);
+                res.redirect('/send-email')
+            }
+        });
+    } catch (error) {
+        console.error('reCAPTCHA verification error: ', error);
+        res.status(500).send('Error verifiying reCAPTCHA');
+    }   
 })
 
 const port = process.env.PORT || 3000;
